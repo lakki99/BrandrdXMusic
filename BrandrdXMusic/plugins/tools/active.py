ab2f4e02753fe211c6d35f327a2304f6e57a7241
Lakki reddy <pulka143341@gmail.com>
2026-05-27 07:20:23 +0530

    Refactor active voice and video chat commands

--- BrandrdXMusic/plugins/tools/active.py
+++ BrandrdXMusic/plugins/tools/active.py
@@ -1,6 +1,5 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from unidecode import unidecode
 
 from BrandrdXMusic import app
 from config import OWNER_ID
@@ -14,29 +13,46 @@ from BrandrdXMusic.utils.database import (
 
 @app.on_message(filters.command(["activevc", "activevoice"]) & filters.user(OWNER_ID))
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text(
+        "» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ..."
+    )
+
     served_chats = await get_active_chats()
     text = ""
     j = 0
+
     for x in served_chats:
         try:
-            title = (await app.get_chat(x)).title
+            chat = await app.get_chat(x)
+            title = chat.title
         except:
             await remove_active_chat(x)
             continue
+
         try:
-            if (await app.get_chat(x)).username:
-                user = (await app.get_chat(x)).username
-                text += f"<b>{j + 1}.</b> <a href=https://t.me/{user}>{unidecode(title).upper()}</a> [<code>{x}</code>]\n"
+            if chat.username:
+                user = chat.username
+                text += (
+                    f"<b>{j + 1}.</b> "
+                    f"<a href=https://t.me/{user}>{title}</a> "
+                    f"[<code>{x}</code>]\n"
+                )
             else:
                 text += (
-                    f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
+                    f"<b>{j + 1}.</b> "
+                    f"{title} "
+                    f"[<code>{x}</code>]\n"
                 )
+
             j += 1
+
         except:
             continue
+
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(
+            f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ {app.mention}."
+        )
     else:
         await mystic.edit_text(
             f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs :</b>\n\n{text}",
@@ -46,29 +62,46 @@ async def activevc(_, message: Message):
 
 @app.on_message(filters.command(["activev", "activevideo"]) & filters.user(OWNER_ID))
 async def activevi_(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text(
+        "» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ..."
+    )
+
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
+
     for x in served_chats:
         try:
-            title = (await app.get_chat(x)).title
+            chat = await app.get_chat(x)
+            title = chat.title
         except:
             await remove_active_video_chat(x)
             continue
+
         try:
-            if (await app.get_chat(x)).username:
-                user = (await app.get_chat(x)).username
-                text += f"<b>{j + 1}.</b> <a href=https://t.me/{user}>{unidecode(title).upper()}</a> [<code>{x}</code>]\n"
+            if chat.username:
+                user = chat.username
+                text += (
+                    f"<b>{j + 1}.</b> "
+                    f"<a href=https://t.me/{user}>{title}</a> "
+                    f"[<code>{x}</code>]\n"
+                )
             else:
                 text += (
-                    f"<b>{j + 1}.</b> {unidecode(title).upper()} [<code>{x}</code>]\n"
+                    f"<b>{j + 1}.</b> "
+                    f"{title} "
+                    f"[<code>{x}</code>]\n"
                 )
+
             j += 1
+
         except:
             continue
+
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(
+            f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ᴏɴ {app.mention}."
+        )
     else:
         await mystic.edit_text(
             f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs :</b>\n\n{text}",
